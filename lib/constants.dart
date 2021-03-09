726fc90644131bfd65f4ba6eaddc08697bd01e2c
Mary Plana <MaryCharmaineLoiPlana@my.unt.edu>
2021-03-08 18:15:17 -0600

    Added images and fonts

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+//UI Colors
+const kYellow = Color(0xffF4ECCC);
+const kDarkBlue = Color(0xff283547);
+const kLightBlue = Color(0xff7093B2);
+const kAccentYellow = Color(0xffFFFBEF);
